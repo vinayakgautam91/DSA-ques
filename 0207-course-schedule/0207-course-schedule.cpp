034a65c465c77d2a043eebaf7055b835876b2fea
Vinayak Gautam <25me01100@iitbbs.ac.in>
2026-09-13 16:06:44 +0530

    Time: 11 ms (21.67%), Space: 20.1 MB (34.66%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,47 +1,41 @@
 class Solution {
 public:
-    bool dfs(int i, vector<int>& visited,
-             vector<vector<int>>& ad, vector<int>& path) {
-
-        visited[i] = 1;
-        path[i] = 1;
-
-        for (int n : ad[i]) {
-
-            if (!visited[n]) {
-                if (!dfs(n, visited, ad, path))
-                    return false;
-            }
-            else if (path[n]) {
-                return false;  // cycle found
-            }
-        }
-
-        path[i] = 0;  
-
-        return true;
-    }
+    
 
     bool canFinish(int numCourses, vector<vector<int>>& pre) {
 
-        vector<int> visited(numCourses, 0);
+        vector<int> indegree(numCourses, 0);
         vector<int> path(numCourses, 0);
         vector<vector<int>> ad(numCourses);
 
         for (auto p : pre) {
             int u = p[0];
             int v = p[1];
-
+            indegree[v]++;
             ad[u].push_back(v);
         }
-
-        for (int i = 0; i < numCourses; i++) {
-            if (!visited[i]) {
-                if (!dfs(i, visited, ad, path))
-                    return false;
+        queue<int> q;
+        for(int i = 0;i<numCourses;i++){
+            if(indegree[i] == 0){
+                q.push(i);
             }
         }
+        int count = 0;
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            count++;
+            for(int n:ad[node]){
+                indegree[n]--;
+                if(indegree[n] == 0){
+                    q.push(n);
+                }
+            }
+        }
+        
+       
 
-        return true;
+        return count == numCourses;
     }
 };
